90489f1fcccd218785a434ed0009459eb1ce5389
Matt McGee <mlmcgeenc@gmail.com>
2022-07-26 11:02:23 -0400

    set up controller files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const sequelize = require('./config/config');
 const helpers = require('./utils/helpers');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
