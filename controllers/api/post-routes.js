f3e76e1158e53eeedf84b26e1bec12aa6a4c3c2a
Matt McGee <mlmcgeenc@gmail.com>
2022-07-27 19:41:46 -0400

    add post routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,4 +3,63 @@ const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/config');
 const withAuth = require('../../utils/auth');
 
-module.exports = router
+// POST api/posts
+router.post('/', (req, res) => {
+	Post.create({
+		title: req.body.title,
+		post_url: req.body.post_url,
+		user_id: req.body.user_id,
+	})
+		.then((dbPostData) => res.json(dbPostData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+// PUT api/posts/id
+router.put('/:id', (req, res) => {
+	Post.update(
+		{
+			title: req.body.title,
+		},
+		{
+			where: {
+				id: req.params.id,
+			},
+		}
+	)
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: 'No post found with the provided id' });
+				return;
+			}
+			res.json(dbPostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+// DELETE api/posts/id
+router.delete('/:id', (req, res) => {
+	Post.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: 'No post found with the provided id' });
+				return;
+			}
+			res.json(dbPostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+module.exports = router;
