78eaead3e82f155b8b5519894185e97e0cc9c418
Matt McGee <mlmcgeenc@gmail.com>
2022-07-27 20:27:58 -0400

    add comment routes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,4 +1,35 @@
 const router = require('express').Router();
 const { Comment, User, Post } = require('../../models');
 
+router.get('/', (req, res) => {
+	Comment.findAll({
+		attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+		include: {
+			model: User,
+			attributes: ['username'],
+		},
+	})
+		.then((dbCommentData) => res.json(dbCommentData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.post('/', (req, res) => {
+	if (req.sesion) {
+		Comment.create({
+			comment_text: req.body.comment_text,
+			post_id: req.body.post_id,
+			created_at: req.body.created_at,
+			user_id: req.session.user_id,
+		})
+			.then((dbCommentData) => res.json(dbCommentData))
+			.catch((err) => {
+				console.log(err);
+				res.status(500).json(err);
+			});
+	}
+});
+
 module.exports = router;
