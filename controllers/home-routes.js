3677cfb018e20abe3829b6651461ee50e3464a18
Matt McGee <mlmcgeenc@gmail.com>
2022-09-19 19:26:48 -0400

    add conditional rendering with handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -66,10 +66,20 @@ router.get('/post/:id', (req, res) => {
 			'title',
 			'created_at',
 		],
-		include:{
-				model: User,
-				attributes: ['username'],
-			},
+		include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
 	})
 		.then((dbPostData) => {
 			if (!dbPostData) {
