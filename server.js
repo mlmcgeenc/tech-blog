40ac2a865fc15958d7f13d2d0744b00d6c73e42c
Matt McGee <mlmcgeenc@gmail.com>
2022-09-20 10:14:42 -0400

    correct error in server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // make the frontend available to the client via middleware
 const path = require('path');
 const express = require('express');
-const helpers = require('./utils/helpers');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
@@ -9,8 +8,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/config');
-// sessions and cookies
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
 	secret: 'Super secret secret',
   cookie: { maxAge: 300000 },
@@ -23,26 +22,24 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
 
-// use Handlebars.js as app's template engine with helper functions available
 const hbs = exphbs.create({ helpers });
 
-// use Handlebars.js as app's template engine
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 // middleware so express knows how to handle incoming data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// handle frontend public files
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const routes = require('./controllers');
 
 // turn on routes
 // routes must be after all static calls
-app.use(routes);
+// app.use(routes);
+app.use(require('./controllers/'));
 
 // turn on connect to db and server
 // force: true resyncs the connections and tables recreated
