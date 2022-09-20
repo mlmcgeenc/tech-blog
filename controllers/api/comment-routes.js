a7af542923bd7099670bee6064b9e9794a8b7659
Matt McGee <mlmcgeenc@gmail.com>
2022-09-19 22:26:05 -0400

    adding comments now works

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -26,19 +26,16 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-	if (req.sesion) {
-		Comment.create({
-			comment_text: req.body.comment_text,
-			post_id: req.body.post_id,
-			created_at: req.body.created_at,
-			user_id: req.session.user_id,
-		})
-			.then((dbCommentData) => res.json(dbCommentData))
-			.catch((err) => {
-				console.log(err);
-				res.status(400).json(err);
-			});
-	}
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.session.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', withAuth, (req, res) => {
